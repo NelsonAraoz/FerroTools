2d28aaaccce8ebcd2cd88d3d17c58fc683525997
araoznelson <nelsonaraoz@ymail.com>
2014-04-01 21:22:07 -0400

    anadiendo validate de imagen

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -4,4 +4,5 @@ has_attached_file :picture,
     :storage => :dropbox,
     :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
     :dropbox_options => {:path =>proc { |style| "Pictures/#{product_id}/#{id}/#{picture.original_filename}" } , :unique_filename => true}
+validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] }
 end
