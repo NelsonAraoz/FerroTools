1f7c6b1b7ec13a293de0f08efaca4f65f5b4ef58
araoznelson <nelsonaraoz@ymail.com>
2014-04-01 21:06:41 -0400

    anadiendo seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,5 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user=User.create(email: "nelsonaraoz@ymail.com",encrypted_password:"$2a$10$yuIkH85VvhWJQuSzn/eHZu5iGUsauZao3LRU9RoYyjHkllYpxTrw2",name: "nelson",lastname: "araoz",phone: "33434",address: "sasd",remember_created_at: "2014-03-22 01:44:45.854526",sign_in_count: "1",rol: "regular")
+user=User.create(email: "admin@ferrotools.com", :password => "1234567e",:confirmed_at => Time.now, :password_confirmation => "1234567e",name: "admin",lastname: "admin",phone: "33434",address: "sasd",rol: "admin")
+#Category.create(nombre: "asdasdasdqwuehuqwheuhaiudsh")
