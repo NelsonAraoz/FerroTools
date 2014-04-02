7eb50ab3235c9a5ba1b3c5a1f5b5d26a3a6b0cee
araoznelson <nelsonaraoz@ymail.com>
2014-04-01 20:46:08 -0400

    mailer en production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,14 @@ CatalogoVentas::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.action_mailer.default_url_options = { :host => 'localhost' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+config.action_mailer.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {  
+  :enable_starttls_auto => true,
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :authentication => :plain,
+    :domain => 'gmail.com',
+    :user_name => 'yosistemasucb@gmail.com',
+    :password => 'tddds2013',}
 end
