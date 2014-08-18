6ec83365909a61e0655abc281f176fffbebe8a37
zuki26 <nelson-araoz@hotmail.com>
2014-08-17 20:52:49 -0400

    prueba heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,8 +3,8 @@ CatalogoVentas::Application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-config.serve_static_assets = true
-config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both thread web servers
   # and those relying on copy on write to perform better.
@@ -21,21 +21,20 @@ config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-
+config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
-
+config.assets.compress = false
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
-config.assets.compress = true
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
