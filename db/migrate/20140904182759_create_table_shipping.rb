8987e20b760c7adbbb52ededd2c5630646c8ca1e
zuki26 <nelson-araoz@hotmail.com>
2014-09-17 10:42:41 -0400

    casi completo

--- db/migrate/20140904182759_create_table_shipping.rb
+++ db/migrate/20140904182759_create_table_shipping.rb
@@ -1,7 +1,6 @@
 class CreateTableShipping < ActiveRecord::Migration
   def change
     create_table :shippings do |t|
-    	 t.references :order, index: true
     	 t.references :deliver, index: true 
     	 t.string :status,:default => 'pending'
     	 t.integer :messenger_id
